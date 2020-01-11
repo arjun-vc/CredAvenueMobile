f7a5b986a01df8bb18e1b3dc55716ef5de6c9a33
Arjun.R <arjun.r@VCAPLT00188.local>
2020-01-12 01:46:55 +0530

    details page

--- CredAvenueMobile/screens/TransactionDetailsScreen.js
+++ CredAvenueMobile/screens/TransactionDetailsScreen.js
@@ -7,21 +7,45 @@ import {
   FlatList,
   View
 } from "react-native";
+import _get from "lodash/get";
 
-const TransactionDetailsScreen = (item) => {
-  return (
-    <View style={{ height: "auto" }}>
-      <Text style={{ textAlign: "center", marginBottom: 10 }}>
-        {_get(item, "clientName")}
-      </Text>
-      <Text style={{ textAlign: "center", color: "skyblue" }}>
-        {_get(item, "dealName")}
-      </Text>
-      <Text style={{ textAlign: "center", color: "red" }}>
-        {_get(item, "transactionType")}
-      </Text>
-    </View>
-  );
+class TransactionDetailsScreen extends React.Component {
+  render() {
+   const { navigation } = this.props;
+    console.log(navigation);
+    return (
+      <View style={{ height: "auto" }}>
+        <Text style={{ textAlign: "center", marginBottom: 10 }}>
+          {JSON.stringify(navigation.getParam("clientName", "NO-ID"))}
+        </Text>
+        <Text style={{ textAlign: "center", color: "skyblue" }}>
+          {JSON.stringify(navigation.getParam("dealName", "NO-ID"))}
+        </Text>
+        <Text style={{ textAlign: "center", color: "red" }}>
+          {JSON.stringify(navigation.getParam("transactionState", "NO-ID"))}
+        </Text>
+        <Text style={{ textAlign: "center" }}>
+          Asset Class -{" "}
+          {JSON.stringify(navigation.getParam("assetClass", "NO-ID"))}
+        </Text>
+        <Text style={{ textAlign: "center" }}>
+          Bid - {JSON.stringify(navigation.getParam("hasBid", "NO-ID"))}
+        </Text>
+        <Text>
+          {JSON.stringify(navigation.getParam("transactionType", "NO-ID"))}
+        </Text>
+        <Text>
+          Amount -{" "}
+          {JSON.stringify(
+            navigation.getParam("humanizedIssuanceSize", "NO-ID")
+          )}
+        </Text>
+        <Text>
+          PoolType - {JSON.stringify(navigation.getParam("poolType", "NO-ID"))}
+        </Text>
+      </View>
+    );
+  }
 }
 
 
